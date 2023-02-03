f1336e6cc3b63d0eca4e3b541bb2a0de074b710e
scalvaruso <calvaruso.simone@gmail.com>
2023-02-03 17:02:02 +0000

    Added parameters: min_width and max_width

--- borders.py
+++ borders.py
@@ -13,33 +13,43 @@ def main():
 
     col = frame(["Enter a colour"], window="in")
     os.system("clear")
+    try:
+        col = int(col)
+    except:
+        pass
 
     frame(menu_list, colour=col)
 
 
-def frame(menu_list, colour=0, spacing=1, window="print"):
+def frame(menu_list, colour=0, spacing=1, min_width=8, max_width=70, window="print"):
     """
     This function create a frame around the content of a list.
     Any item of the list is considered a new line.
     
     Parameters:
-        colour: allows to change the colour of the window
+        colour: allows to change the colour of the window.
             default value = 'white'
-        spacing: allows to increase or decrease the space between the frame and the text
+        spacing: allows to increase or decrease the space between the frame and the text.
             default value = 1
-        window: allows the function to behave as input
+        min_width: allows to set the minimum width inside the frame.
+            default value = 8
+        max_width: allows to set the max length of text on a line.
+            default value = 70
+        window: allows the function to behave as input.
             default value = 'print'
 
     Returns:
         Print a frame around the output or the input prompt
     """
 
-    # Set the colour of the frame.
+    ansi_colours = [0,30,31,32,33,34,35,36,37,90,91,92,93,94,95,96,97,
+    40,41,42,43,44,45,46,47,100,101,102,103,104,105,106,107]
 
+    # Set the colour of the frame.
     if type(colour) == str:
         colour = colour.lower()
-
-    if colour in [0,91,92,93,96,94,95]:
+    
+    if colour in ansi_colours:
         pass
     elif colour == "red":
         colour = "91"
@@ -58,16 +68,22 @@ def frame(menu_list, colour=0, spacing=1, window="print"):
 
     new_list = []
 
-    # Split the lines if they are longer than 70 characters.
-
+    # Check the menu spacing and width.
     if spacing < 1:
         border_space = 0
-        menu_width = 50
+        menu_width = 0
     else:
         border_space = spacing * 4
-        menu_width = 58
+        menu_width = min_width
     side_space = border_space * " "
+    
+    if max_width < min_width:
+        max_width = min_width
+    
+    if max_width < 5:
+        max_width = 5
 
+    # Split the lines if they are longer than 70 characters.
     for item in menu_list:
         line = ""
         words = item.split(" ")
@@ -75,11 +91,11 @@ def frame(menu_list, colour=0, spacing=1, window="print"):
         for word in words:
             word = word.replace("\t", "    ")
             
-            if len(word) >= 70:
-                line = word[:70]
+            if len(word) >= max_width:
+                line = word[:max_width]
                 new_list.append(line.rstrip(" "))
-                line = word[70:] + space
-            elif len(line+word) < 71:
+                line = word[max_width:] + space
+            elif len(line+word) < (max_width+1):
                 line += word + space
             else:
                 new_list.append(line.rstrip(" "))
@@ -90,7 +106,6 @@ def frame(menu_list, colour=0, spacing=1, window="print"):
     menu_list = new_list
 
     # Create the frame.
-
     max_width = max([(len(i.rstrip(" "))) for i in menu_list])
 
     if max_width > (menu_width-(border_space*2)):
@@ -100,7 +115,7 @@ def frame(menu_list, colour=0, spacing=1, window="print"):
     filling = " " * menu_width
     display_menu = f"\033[{colour}m" + "╔" + or_line + "╗\n"
 
-    for i in range(spacing):
+    for _ in range(spacing):
         display_menu += "║" + filling + "║\n"
 
     for item in menu_list:
@@ -111,13 +126,12 @@ def frame(menu_list, colour=0, spacing=1, window="print"):
 
     filling = " " * menu_width
 
-    for i in range(spacing):
+    for _ in range(spacing):
         display_menu += "║" + filling + "║\n"
 
     display_menu += "╚" + or_line + "╝\033[0m\n"
 
     # Change the behaviour of the function from 'print' to 'input'.
-
     if window=="in" or window=="input":
         return input(display_menu)
     else:
