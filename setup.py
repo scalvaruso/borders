f06f55aa8c3d4289bcbc0b1aeb5a82d7210b1997
scalvaruso <calvaruso.simone@gmail.com>
2023-11-16 21:50:08 +0000

    version==1.1.0

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    # Other setup configurations
+
+    # Use find_packages and exclude the folder
+    packages=find_packages(exclude=['images']),
+)
