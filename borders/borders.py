316ffc421fd4bb347efc49ba0d98f2478c6bf78b
scalvaruso <calvaruso.simone@gmail.com>
2023-12-27 18:47:11 +0000

    Version-1.2.0

--- borders/borders.py
+++ borders/borders.py
@@ -1,5 +1,7 @@
 import os
 from textlinebreaker import split_line
+from polychromy import colorate
+
 
 def main():
 
@@ -14,35 +16,42 @@ def main():
         "Any item of the list is considered a new line.",
         "Items allowed are strings or tuples containing:",
         "a string with text to print,",
-        "one or two values (integers or strings) for foreground and background colours of the text,",
+        "one or two values (integers or strings) for foreground and background colors of the text,",
         "one string value for the alignment of the line."
         ]
     menu_2 = [
         ("Parameters:","","Bright Red","centre"),"",
-        ("colour:","Bright Red","centre"), "allows to change the colour of the text.",
+        ("colour:","Bright Red","centre"), "allows to change the color of the text.",
         "default value = 37",
-        "allowed values:",
-        "0, 30, 31, 32, 33, 34, 35, 36, 37, 90, 91, 92, 93, 94, 95, 96, 97,",
-        "'Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White',",
-        "'Bright Black', 'Bright Red', 'Bright Green', 'Bright Yellow', 'Bright Blue', 'Bright Magenta', 'Bright Cyan', 'Bright White'",
-        ("text_background:","Bright Red","centre"), "allows to change the background colour of the text.",
+        "allowed values: most color names",
+        "RGB values [0-255];[0-255];[0-255]",
+        "Hex values #[00-FF][00-FF][00-FF]",
+        "xterm color number in the format x[0-255]",
+        "and ANSI codes 0, [30-37], [90-97]",
+        ("text_background:","Bright Red","centre"), "allows to change the background color of the text.",
         "default value = 0",
-        "allowed values:",
-        "0, 40, 41, 42, 43, 44, 45, 46, 47, 100, 101, 102, 103, 104, 105, 106, 107",
-        "'Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White',",
-        "'Bright Black', 'Bright Red', 'Bright Green', 'Bright Yellow', 'Bright Blue', 'Bright Magenta', 'Bright Cyan', 'Bright White'",
-        ("frame_colour:","Bright Red","centre"), "allows to change the colour of the frame.",
-        "default value = None",("(if not specified get the same colour value of the text)","right"),
-        "allowed values:",
-        "0, 30, 31, 32, 33, 34, 35, 36, 37, 90, 91, 92, 93, 94, 95, 96, 97",
-        "'Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White',",
-        "'Bright Black', 'Bright Red', 'Bright Green', 'Bright Yellow', 'Bright Blue', 'Bright Magenta', 'Bright Cyan', 'Bright White'",
-        ("frame_background:","Bright Red","centre"), "allows to change the background colour of the frame.",
+        "allowed values: most color names",
+        "RGB values [0-255];[0-255];[0-255]",
+        "Hex values #[00-FF][00-FF][00-FF]",
+        "xterm color number in the format x[0-255]",
+        "and ANSI codes 0, [40-47], [100-107]",
+        ("frame_colour:","Bright Red","centre"), "allows to change the color of the frame.",
+        "default value = None",("(if not specified get the same color value of the text)","right"),
+        "allowed values: most color names",
+        "RGB values [0-255];[0-255];[0-255]",
+        "Hex values #[00-FF][00-FF][00-FF]",
+        "xterm color number in the format x[0-255]",
+        "and ANSI codes 0, [30-37], [90-97]",
+        ("frame_background:","Bright Red","centre"), "allows to change the background color of the frame.",
         "default value = None",("(if not specified get the same value of text_background).","right"),
-        "allowed values:",
-        "0, 40, 41, 42, 43, 44, 45, 46, 47, 100, 101, 102, 103, 104, 105, 106, 107",
-        "'Black', 'Red', 'Green', 'Yellow', 'Blue', 'Magenta', 'Cyan', 'White',",
-        "'Bright Black', 'Bright Red', 'Bright Green', 'Bright Yellow', 'Bright Blue', 'Bright Magenta', 'Bright Cyan', 'Bright White'",
+        "allowed values: most color names",
+        "RGB values [0-255];[0-255];[0-255]",
+        "Hex values #[00-FF][00-FF][00-FF]",
+        "xterm color number in the format x[0-255]",
+        "and ANSI codes 0, [40-47], [100-107]",
+        ("frame_style:","Bright Red","centre"), "allows to change the style of the frame",
+        "default value = 'double'",
+        "allowed values: 'single', 'double', 'double horizontal', 'double vertical', 'dots', None",
         ("alignment:","Bright Red","centre"), "allows to change the alignment of the text inside the frame.",
         "default value = 'left'",
         "allowed values: 'left', 'centre', 'center', 'right'",
@@ -70,36 +79,47 @@ def main():
     frame(menu_2, text_background=0, frame_colour="Blue", frame_background=0, min_width=75, display="centre", spacing=1)
 
 
-def frame(menu_list, colour=37, text_background=0, frame_colour=None, frame_background=None, alignment="left", display="left", spacing=1, min_width=42, max_width=70, window="print"):
+def frame(menu_list, colour=37, text_background=0, frame_colour=None, frame_background=None, frame_style="double", alignment="left", display="left", spacing=1, min_width=42, max_width=70, window="print"):
     """
     This function create a frame around the content of a list.
     Any item of the list is considered a new line.
     Items allowed are strings or tuples containing:
     a string with text to print,
-    one or two values (integers or strings) for foreground and background colours of the text,
+    one or two values (integers or strings) for foreground and background colors of the text,
     one string value for the alignment of the line.
     
     Parameters:
-        colour: allows to change the colour of the text.
+        colour: allows to change the color of the text.
             default value = 37
-            allowed values: 0, 30, 31, 32, 33, 34, 35, 36, 37, 90, 91, 92, 93, 94, 95, 96, 97,
-                            "Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White",
-                            "Bright Black", "Bright Red", "Bright Green", "Bright Yellow", "Bright Blue", "Bright Magenta", "Bright Cyan", "Bright White"
-        text_background: allows to change the background colour of the text.
+            allowed values: "allowed values: most color names"
+                            "RGB values [0-255];[0-255];[0-255]"
+                            "Hex values #[00-FF][00-FF][00-FF]"
+                            "xterm color number in the format x[0-255]"
+                            "and ANSI codes 0, [30-37], [90-97]"
+        text_background: allows to change the background color of the text.
             default value = 0
-            allowed values: 0, 40, 41, 42, 43, 44, 45, 46, 47, 100, 101, 102, 103, 104, 105, 106, 107
-                            "Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White",
-                            "Bright Black", "Bright Red", "Bright Green", "Bright Yellow", "Bright Blue", "Bright Magenta", "Bright Cyan", "Bright White"
-        frame_colour: allows to change the colour of the frame.
-            default value = None (if not specified get the same colour value of the text).
-            allowed values: 0, 30, 31, 32, 33, 34, 35, 36, 37, 90, 91, 92, 93, 94, 95, 96, 97
-                            "Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White",
-                            "Bright Black", "Bright Red", "Bright Green", "Bright Yellow", "Bright Blue", "Bright Magenta", "Bright Cyan", "Bright White"
-        frame_background: allows to change the background colour of the frame.
+            allowed values: "allowed values: most color names,"
+                            "RGB values [0-255];[0-255];[0-255],"
+                            "Hex values #[00-FF][00-FF][00-FF],"
+                            "xterm color number in the format x[0-255],"
+                            "and ANSI codes 0, [40-47], [100-107]."
+        frame_colour: allows to change the color of the frame.
+            default value = None (if not specified get the same color value of the text).
+            allowed values: "allowed values: most color names"
+                            "RGB values [0-255];[0-255];[0-255]"
+                            "Hex values #[00-FF][00-FF][00-FF]"
+                            "xterm color number in the format x[0-255]"
+                            "and ANSI codes 0, [30-37], [90-97]"
+        frame_background: allows to change the background color of the frame.
             default value = None (if not specified get the same value of text_background).
-            allowed values: 0, 40, 41, 42, 43, 44, 45, 46, 47, 100, 101, 102, 103, 104, 105, 106, 107
-                            "Black", "Red", "Green", "Yellow", "Blue", "Magenta", "Cyan", "White",
-                            "Bright Black", "Bright Red", "Bright Green", "Bright Yellow", "Bright Blue", "Bright Magenta", "Bright Cyan", "Bright White"
+            allowed values: "allowed values: most color names,"
+                            "RGB values [0-255];[0-255];[0-255],"
+                            "Hex values #[00-FF][00-FF][00-FF],"
+                            "xterm color number in the format x[0-255],"
+                            "and ANSI codes 0, [40-47], [100-107]."
+        frame_style: allows to change the style of the frame
+            default value = "double"
+            allowed values: "single", "double", "double horizontal", "double vertical", "dots", None
         alignment: allows to change the alignment of the text inside the frame.
             default value = "left"
             allowed values: "left", "centre", "center", "right"
@@ -122,22 +142,22 @@ def frame(menu_list, colour=37, text_background=0, frame_colour=None, frame_back
     Returns:
         Print a frame around the output or the input prompt.
     """
-    # Check validity of the colours.
+    # Check validity of the colors.
         
-    colour = _valid_colour(colour)
-    text_background = _valid_colour(text_background, "back")
+    color = colour
+    # text_background = text_background
     
     if frame_colour:
-        frame_colour = _valid_colour(frame_colour)
+        frame_color = frame_colour
     else:
-        frame_colour = colour
+        frame_color = color
 
     if frame_background:
-        frame_background = _valid_colour(frame_background, "back")
+        pass # frame_background = frame_background
     else:
         frame_background = text_background
 
-    t_colour = colour
+    t_color = color
     t_background = text_background
 
     # Retrieve terminal width
@@ -195,15 +215,15 @@ def frame(menu_list, colour=37, text_background=0, frame_colour=None, frame_back
 
     for item in menu_list:
         
-        # Check if item is a tuple, and get values for foreground, and background colours (t_colour, t_background)
+        # Check if item is a tuple, and get values for foreground, and background colors (t_color, t_background)
         if isinstance(item, tuple):
             parameters = len(item)
             
             """
             If True:
             item[0] is the string.
-            item[1] is the colour of the text.
-            item[2] is the backgroud colour of the text.
+            item[1] is the color of the text.
+            item[2] is the backgroud color of the text.
             item[3] is the line alignment.
             """
             
@@ -217,32 +237,32 @@ def frame(menu_list, colour=37, text_background=0, frame_colour=None, frame_back
                     line_alignment = item[1]
                 else:
                     if item[1] != "":
-                        t_colour = _valid_colour(item[1])
+                        t_color = item[1]
                     else:
-                        t_colour = colour
+                        t_color = color
                     line_alignment = alignment
 
             elif parameters == 3:
                 if item[1] != "":
-                    t_colour = _valid_colour(item[1])
+                    t_color = item[1]
                 else:
-                    t_colour = colour
+                    t_color = color
                 if str(item[2]).lower() in ("left","center","centre","right"):
                     line_alignment = item[2]
                 else:
                     if item[2] != "":
-                        t_background = _valid_colour(item[2], "back")
+                        t_background = item[2]
                     else:
                         t_background = text_background
                     line_alignment = alignment
 
             else:
                 if item[1] != "":
-                    t_colour = _valid_colour(item[1])
+                    t_color = item[1]
                 else:
-                    t_colour = colour
+                    t_color = color
                 if item[2] != "":
-                    t_background = _valid_colour(item[2], "back")
+                    t_background = item[2]
                 else:
                     t_background = text_background
                 if str(item[3]).lower() in ("left","center","centre","right"):
@@ -250,21 +270,21 @@ def frame(menu_list, colour=37, text_background=0, frame_colour=None, frame_back
                 else:
                     line_alignment = alignment
             
-        # If item is not a tuple, default colours are assigned.
+        # If item is not a tuple, default colors are assigned.
         else:
             if item != "":
                 words = item.split(" ")
             else:
                 words = item
             #words = item.split(" ")
-            t_colour = colour
+            t_color = color
             t_background = text_background
             line_alignment = alignment
 
         # The function split_line is applied to every element in "menu_list"
         for line in split_line(words, max_length, line_alignment):
-            # Then the colours attributes for foreground, and background are applied
-            new_list.append((line,t_colour,t_background))
+            # Then the colors attributes for foreground, and background are applied
+            new_list.append((line,t_color,t_background))
 
     # Create the frame.
     max_width = max([(len(i[0])) for i in new_list])
@@ -272,11 +292,15 @@ def frame(menu_list, colour=37, text_background=0, frame_colour=None, frame_back
     if max_width > (menu_width-(border_space*2)):
         menu_width = max_width + (border_space*2)
 
-    or_line = "═" * menu_width
+    # Defining the elements of the frame:
+    # Top Left Corner, Horizontal line (single element), Top Right Corner, Vertical line, Bottom Left Corner, Bottom Right Corner.
+    tl_corner, h_line, tr_corner, v_line, bl_corner, br_corner = _elements(frame_style)
+    # Horizontal Line (Full length)
+    hor_line = h_line * menu_width
+    # Empty line
     filling = " " * menu_width
 
     # Check the frame positioning.
-
     if display.lower() == "right":
         positioning = terminal_width - (menu_width+2)
     elif display.lower()=="centre" or display.lower()=="center":
@@ -286,18 +310,18 @@ def frame(menu_list, colour=37, text_background=0, frame_colour=None, frame_back
     adjustment = " " * positioning
 
     # Create the frame
-    display_menu = adjustment + f"\033[{frame_background};{frame_colour}m" + "╔" + or_line + "╗" + "\033[0m" + "\n"
+    display_menu = adjustment + colorate(tl_corner + hor_line + tr_corner, frame_color, frame_background) + "\n"
    
     for _ in range(round(spacing)):
-        display_menu += adjustment + f"\033[{frame_background};{frame_colour}m" + "║" + f"\033[{text_background}m" + filling + f"\033[0m" + f"\033[{frame_background};{frame_colour}m" + "║" +"\033[0m\n"
+        display_menu += adjustment + colorate(v_line, frame_color, frame_background) + colorate(filling, 0, text_background) + colorate(v_line, frame_color, frame_background) + "\n"
        
     for item in new_list:
-        display_menu += adjustment + f"\033[{frame_background};{frame_colour}m" + "║" + f"\033[{item[2]}m" + side_space + f"\033[{item[1]}m" + item[0] + side_space + f"\033[0m" + f"\033[{frame_background};{frame_colour}m" + "║" +"\033[0m\n"
+        display_menu += adjustment + colorate(v_line, frame_color, frame_background) + colorate(side_space+ item[0] + side_space, item[1], item[2]) + colorate(v_line, frame_color, frame_background) +"\n"
         
     for _ in range(round(spacing)):
-        display_menu += adjustment + f"\033[{frame_background};{frame_colour}m" + "║" + f"\033[{text_background}m" + filling + f"\033[0m" + f"\033[{frame_background};{frame_colour}m" + "║" + "\033[0m\n"
+        display_menu += adjustment + colorate(v_line, frame_color, frame_background) + colorate(filling, 0, text_background) + colorate(v_line, frame_color, frame_background) + "\n"
         
-    display_menu += adjustment + f"\033[{frame_background};{frame_colour}m" + "╚" + or_line + "╝" + "\033[0m\n"
+    display_menu += adjustment + colorate(bl_corner + hor_line + br_corner, frame_color, frame_background) + "\n"
    
     # Change the behaviour of the function from 'print' to 'input'.
     if window=="in" or window=="input":
@@ -306,36 +330,29 @@ def frame(menu_list, colour=37, text_background=0, frame_colour=None, frame_back
         print(display_menu)
 
 
-# This function check the validity of the colour value.
-def _valid_colour(col, body="body"):
-    
-    if body == "back":
-        ansi_colours = {
-    "Black": 40, "Red": 41, "Green": 42, "Yellow": 43, "Blue": 44, "Magenta": 45, "Cyan": 46, "White": 47,
-    "Bright Black": 100, "Bright Red": 101, "Bright Green": 102, "Bright Yellow": 103, "Bright Blue": 104, "Bright Magenta": 105, "Bright Cyan": 106, "Bright White": 107,
-    "Reset": 0,
-    }
-    else:
-        ansi_colours = {
-    "Black": 30, "Red": 31, "Green": 32, "Yellow": 33, "Blue": 34, "Magenta": 35, "Cyan": 36, "White": 37,
-    "Bright Black": 90, "Bright Red": 91, "Bright Green": 92, "Bright Yellow": 93, "Bright Blue": 94, "Bright Magenta": 95, "Bright Cyan": 96, "Bright White": 97,
-    "Reset": 0,
-    }
-   
-    try:
-        col = int(col)
-    except:
-        col = col.title().replace("Light","Bright")
-    
-    if col in ansi_colours.values():
-        out_col = col
-    elif col in ansi_colours.keys():
-        out_col = ansi_colours[col]
-    else:
-        out_col = "0"
+# Defining the elements of the frame
+def _elements(style):
+    # According to the given style value, the function returns:
+    # Top Left Corner, Horizontal line (single element), Top Right Corner, Vertical line, Bottom Left Corner, Bottom Right Corner.
 
-    return out_col
+    if style == "single":
+        return "┌", "─", "┐", "│", "└", "┘" # tl_corner, h_line, tr_corner, v_line, bl_corner, br_corner
+
+    elif style == "double horizontal":
+        return "╒", "═", "╕", "│", "╘", "╛" # tl_corner, h_line, tr_corner, v_line, bl_corner, br_corner
+   
+    elif style == "double vertical":
+        return "╓", "─", "╖", "║", "╙", "╜" # tl_corner, h_line, tr_corner, v_line, bl_corner, br_corner
+
+    elif style == "double":
+        return "╔", "═", "╗", "║", "╚", "╝" # tl_corner, h_line, tr_corner, v_line, bl_corner, br_corner
     
+    elif style == "dots":
+        return "·", "·", "·", ":", "·", "·" # tl_corner, h_line, tr_corner, v_line, bl_corner, br_corner
+
+    else:
+        return (6 * " ")    # tl_corner, h_line, tr_corner, v_line, bl_corner, br_corner
+
 
 if __name__ == "__main__":
     main()
